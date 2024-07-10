4ff12c637aeebf5dba82e2788643bb1f8d48e64a
Sergei Kalinin <nevisdale@gmail.com>
2024-07-10 13:29:44 -0700

    main: create cpu

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/nevisdale/nes-emulator/internal/cpu"
+)
+
+func main() {
+	cpu, err := cpu.NewCPU()
+	if err != nil {
+		log.Fatalf("couldn't create cpu: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	_ = cpu
 }
