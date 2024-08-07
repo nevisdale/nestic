d3f3e92ceace1be88d1babc41e0b2059541ad1b3
Sergei Kalinin <nevisdale@gmail.com>
2024-08-07 16:29:44 +0700

    fix ram

--- internal/nes/ram.go
+++ internal/nes/ram.go
@@ -10,10 +10,10 @@ func NewRAM() *RAM {
 	return &RAM{}
 }
 
-func (r RAM) Read8(addr uint16) uint8 {
+func (r *RAM) Read8(addr uint16) uint8 {
 	return r.ram[addr]
 }
 
-func (r RAM) Write8(addr uint16, data uint8) {
+func (r *RAM) Write8(addr uint16, data uint8) {
 	r.ram[addr] = data
 }
