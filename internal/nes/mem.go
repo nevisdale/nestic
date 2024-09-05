58e8dd0ce1b869a3657af76c6c5b0f879981537e
Sergei Kalinin <nevisdale@gmail.com>
2024-09-06 06:51:55 +0700

    fix ppu memory

--- internal/nes/mem.go
+++ internal/nes/mem.go
@@ -1,7 +1,5 @@
 package nes
 
-import "log"
-
 type ReadWriter interface {
 	Read8(addr uint16) uint8
 	Write8(addr uint16, data uint8)
@@ -39,67 +37,63 @@ func (b *Bus) newCpuMemory() *cpuMemory {
 
 func (c cpuMemory) Read8(addr uint16) uint8 {
 	switch {
-	// read from ram
 	case addr < 0x2000:
 		return c.bus.ram.Read8(addr & 0x07FF)
-	// read from ppu
 	case addr < 0x4000:
 		return c.bus.ppu.readRegister(addr & 0x7)
-	// read from apu
 	case addr < 0x4018:
 		return 0
-	// read from io
 	case addr < 0x4020:
 		return 0
-	// read from cartridge
 	case addr <= 0xFFFF:
 		return c.bus.cart.Read8(addr)
 	}
-
-	log.Fatalln("cpuMemory: unhandled read8 at address", addr)
 	return 0
 }
 
 func (c *cpuMemory) Write8(addr uint16, data uint8) {
 	switch {
-	// write to ram
 	case addr < 0x2000:
 		c.bus.ram.Write8(addr&0x07FF, data)
-		return
-	// write to ppu
 	case addr < 0x4000:
 		c.bus.ppu.writeRegister(addr&0x7, data)
-		return
-	// write to apu
 	case addr < 0x4018:
-		return
-	// write to io
 	case addr < 0x4020:
-		return
-		// write to cartridge
 	case addr <= 0xFFFF:
 		c.bus.cart.Write8(addr, data)
-		return
 	}
-
-	log.Fatalln("cpuMemory: unhandled write8 at address", addr)
 }
 
-// $0000-$0FFF: Pattern table 0
-// $1000-$1FFF: Pattern table 1
-// $2000-$23FF: Nametable 0
-// $2400-$27FF: Nametable 1
-// $2800-$2BFF: Nametable 2
-// $2C00-$2FFF: Nametable 3
-// $3000-$3EFF: Mirrors of $2000-$2FFF
-// $3F00-$3F1F: Palette RAM indexes
-// $3F20-$3FFF: Mirrors of $3F00-$3F1F
+// PPU MEMORY MAP
+//
+// MIRRORS
+// 0x4000 - 0x10000: Mirrors of 0x0000-0x4000
+//
+// PALLETTES
+// 0x3F20 - 0x4000: Mirrors of 0x3F00-0x3F20
+// 0x3F10 - 0x3F20: Sprite palette
+// 0x3F00 - 0x3F10: Image palette
+//
+// NAME TABLES
+// 0x3000 - 0x3F00: Mirrors of 0x2000-0x3000
+// 0x2FC0 - 0x3000: Attribute table 3
+// 0x2C00 - 0x2FC0: Name table 3
+// 0x2BC0 - 0x2C00: Attribute table 2
+// 0x2800 - 0x2BC0: Name table 2
+// 0x27C0 - 0x2800: Attribute table 1
+// 0x2400 - 0x27C0: Name table 1
+// 0x23C0 - 0x2400: Attribute table 0
+// 0x2000 - 0x23C0: Name table 0
+//
+// PATTERN TABLES
+// 0x1000 - 0x2000: Pattern table 1
+// 0x0000 - 0x1000: Pattern table 0
 type ppuMemory struct {
 	bus *Bus
 }
 
-func (b Bus) newPpuMemory() *ppuMemory {
-	return &ppuMemory{bus: &b}
+func (b *Bus) newPpuMemory() *ppuMemory {
+	return &ppuMemory{bus: b}
 }
 
 func (p ppuMemory) Read8(addr uint16) uint8 {
@@ -107,14 +101,8 @@ func (p ppuMemory) Read8(addr uint16) uint8 {
 	switch {
 	case addr < 0x2000:
 		return p.bus.cart.Read8(addr)
-	case addr < 0x3F00:
-	case addr < 0x4000:
-		addr &= 0x1F
-		// Palette mirroring
-		if addr >= 0x10 {
-			addr -= 0x10
-		}
-		return p.bus.ppu.tablePallete[addr]
+	case addr >= 0x3F00:
+		return p.bus.ppu.readPalette(addr)
 	}
 	return 0
 }
@@ -122,18 +110,7 @@ func (p ppuMemory) Read8(addr uint16) uint8 {
 func (p *ppuMemory) Write8(addr uint16, data uint8) {
 	addr &= 0x3FFF
 	switch {
-	case addr < 0x2000:
-		p.bus.cart.Write8(addr, data)
-		return
-	case addr < 0x3F00:
-		return
-	case addr < 0x4000:
-		addr &= 0x1F
-		// Palette mirroring
-		if addr >= 0x10 {
-			addr -= 0x10
-		}
-		p.bus.ppu.tablePallete[addr] = data
-		return
+	case addr >= 0x3F00:
+		p.bus.ppu.writePalette(addr, data)
 	}
 }
